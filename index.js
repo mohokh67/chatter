53266bb715dbab86fa74a8e7d298e005da386f52
MoHo <habibsandyfan@gmail.com>
2018-02-18 11:42:57 +0000

    Save to DB

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-const app       = require('express')();
-const server    = require('http').Server(app);
-const io        = require('socket.io')(server);
-const port      = 3001;
+const app           = require('express')();
+const express       = require('express');
+const server        = require('http').Server(app);
+const io            = require('socket.io')(server);
+const mongoose      = require('mongoose')
+var bodyParser      = require('body-parser');
 
-const express   = require('express');
+const dbURL         = 'mongodb://root:Password@ds012178.mlab.com:12178/chatter';
+const port          = 2002;
+
+
 app.use(express.static(__dirname + '/public/'));
+app.use(bodyParser.json()) // Parse the body from get request
+app.use(bodyParser.urlencoded({extended: false})) // Parse the body of post request from jQuery
 
+mongoose.Promise = Promise; // Use the ES6 promise for mongoose promise
+
+// DB Model
+var Message = mongoose.model('Message', {
+    room_id: Number,
+    name: String,
+    message: String, 
+})
+
+var Room = mongoose.model('Room', {
+    name: String,
+    extra: String, 
+})
+
+// Connect to DB
+mongoose.connect(dbURL, (error) => {
+    console.log('DB error', error); 
+})
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
@@ -14,17 +39,35 @@ app.get('/', (req, res) => {
     //res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/messages', (req, res) => {
+    Message.find({}, (error, messages) => {
+        res.send(messages);
+    })
+});
+
 // tech namespace
 const tech = io.of('/tech');
-tech.on('connection', (socket)=>{
-    console.log('User connected');
-    socket.on('message', (message) => {
-        console.log(`message ${message}`);
-        tech.emit('message', message);
-    });
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-        tech.emit('message', 'user disconnected')     
-    })
+
+io.on('connection', (socket)=>{
+    try{
+        socket.on('message', async (message) => {
+            var thisMessage = new Message(message);
+            var sevedMessage = await thisMessage.save();
+            console.log('Messaged saved to DB');
+        });
+        
+        socket.on('systemLog', (message) => {
+            console.log(`message ${message}`);
+        });
+
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+        })
+
+    } catch(error){
+        return console.error(error);
+    } 
+
+    
 })
 
--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,54 @@
-const socket = io('/tech');
+const socket = io();
 
+$(() => {
+    getMessages();
+    $("html, body").animate({ scrollTop: $(document).height() }, 500);
+})
         $('form').submit(() => {
             var senderMessage = $('#message');
             var sender = $('#sender');
-            var meesage = '<strong>'+ sender.val() +'</strong>: <span>'+ senderMessage.val() +'</span>'
-            socket.emit('message', meesage);
+            //var meesage = '<strong>'+ sender.val() +'</strong>: <span>'+ senderMessage.val() +'</span>'
+            var message = {
+                name: sender.val(),
+                message: senderMessage.val(),
+                room_id: "1"
+            }
+            // postMessage(tempMessage);
+            socket.emit('message', message);
+            addMessage(message);
+
             senderMessage.val('');
-            //sender.prop('disabled', true);
             sender.hide();
             return false;
         });
 
         socket.on('connect', () => {
             //emiting to everyone
-            socket.emit('message', 'user connected');
+            socket.emit('systemLog', 'user connected');
         })
 
-        socket.on('message', (message) => {
-            var messagesContainer = $('#messages');
-            messagesContainer.append($('<li>').html(message));
-        })
+        // socket.on('message', (message) => {
+        //     var messagesContainer = $('#messages');
+        //     messagesContainer.append($('<li>').html(message));
+        // })
 
         // socket.on('message', (data) => {
         //     console.log(data);
         //     socket.emit('another event', { Habib: 'I am good thank you.' });
         // })
+
+
+function getMessages(){
+    $.get('/messages', (messages) => {
+        messages.forEach(addMessage);
+    })
+} 
+
+function addMessage(message){
+    var thisMeesage = '<strong>'+ message.name +'</strong>: <span>'+ message.message +'</span>'
+    $('#messages').append($('<li>').html(thisMeesage));
+}
+
+// function postMessage(message){
+//     $.post('/message', message)
+// }
